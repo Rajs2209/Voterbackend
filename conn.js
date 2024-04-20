05f133e8ba78231aa2101e000d1aa2caa99131c4
Rajs2209 <raj.sah.2209@gmail.com>
2024-04-21 04:16:15 +0530

    backend

--- conn.js
+++ conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const DB = "mongodb+srv://rajsah2209:PTvFZctru4NULlaN@cluster0.tts7zo6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const DB = "mongodb+srv://rajsah2209:PTvFZctru4NULlaN@cluster0.tts7zo6.mongodb.net/?retryWrites=true&w=majority"
 
 mongoose.connect(DB).then(() => {
     console.log(`connection successful`);
