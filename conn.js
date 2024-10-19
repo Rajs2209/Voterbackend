cd636fc666fd19554ab52d3ffd586845f7705026
unknown <raj.sah.2209@gmail.com>
2024-10-19 18:19:48 +0530

    added origin

--- conn.js
+++ conn.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const MONGODB_URL = "mongodb+srv://rajsah2209:PTvFZctru4NULlaN@cluster0.tts7zo6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
+const origin="https://votercertificate.vercel.app/"
 mongoose.connect(MONGODB_URL).then(() => {
     console.log(`connection successful`);
 }).catch((err) => console.log(`no connection ${err}`));
