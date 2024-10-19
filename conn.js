d26e12bf3a9e71b98b2890546289f49c87a5cab5
unknown <raj.sah.2209@gmail.com>
2024-10-19 16:33:05 +0530

    changes in cors

--- conn.js
+++ conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGODB_URL = "mongodb+srv://rajsah2209:PTvFZctru4NULlaN@cluster0.tts7zo6.mongodb.net/?retryWrites=true&w=majority"
+const MONGODB_URL = "mongodb+srv://rajsah2209:PTvFZctru4NULlaN@cluster0.tts7zo6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 mongoose.connect(MONGODB_URL).then(() => {
     console.log(`connection successful`);
